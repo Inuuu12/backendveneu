ee6c3635b5c01ee54321e0d79ae7908fcda6023e
Ridho Putra Pratama <ridhoputra714@gmail.com>
2026-05-09 19:21:24 +0700

    Fetching Users to Frontend

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-
+const cors = require('cors'); 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  credentials: true
+}));
+
 app.use(express.json());
 
 // ROUTES
--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+
 app.use(express.json());
 
 // auth routes
